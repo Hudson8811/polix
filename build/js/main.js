06b82ecab2ef4474cc395f1bd84107b0e7e69001
Toolnado <Toolnado@gmail.com>
2020-02-13 03:49:27 +0300

    changed

--- build/js/main.js
+++ build/js/main.js
@@ -62,6 +62,29 @@ $('.responsive').slick({
     // instead of a settings object
   ]
 });
+
+$('.responsive-2').slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay: false,
+  arrow: true,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 781,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 $(document).ready(function () {
   if (document.documentElement.clientWidth < 780) {
     botContainer.append(bluBtn)
